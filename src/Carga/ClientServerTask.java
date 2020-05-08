1881c3ece4b0f443aef66e732f9f4ed140476d1a
FelipeGuzman <fg.guzman@uniandes.edu.co>
2020-05-08 12:34:35 -0500

    creado el generador de carga

---
+++ src/Carga/ClientServerTask.java
@@ -0,0 +1,27 @@
+package Carga;
+
+import uniandes.gload.core.Task;
+import uniandes.gload.examples.clientserver.Client;
+
+public class ClientServerTask extends Task{
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		Client client = new Client();
+		client.sendMessageToServer("Hi! Im a client");
+		client.waitForMessageFromServer();
+	}
+
+	@Override
+	public void fail() {
+		// TODO Auto-generated method stub
+		System.out.println(Task.MENSAJE_FAIL);
+	}
+
+	@Override
+	public void success() {
+		// TODO Auto-generated method stub
+		System.out.println(Task.OK_MESSAGE);
+	}
+}
